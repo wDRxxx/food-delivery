9a7a02be6789a392046646ed64cbb242db6d3168
Ivan Chebotkov <ivancheb06@mail.ru>
2025-04-26 14:41:03 +0300

    signup done

--- app/src/main/java/com/example/fooddelivery/fragments/SignupFragment.kt
+++ app/src/main/java/com/example/fooddelivery/fragments/SignupFragment.kt
@@ -7,19 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import com.example.fooddelivery.R
 
-class LoginFragment : Fragment() {
+class SignupFragment : Fragment() {
 
     private lateinit var editEmail: EditText
     private lateinit var editPassword: EditText
-    private lateinit var loginBtn: Button
-    private lateinit var forgotPasswordBtn: TextView
-    private lateinit var signupBtn: TextView
+    private lateinit var editRePassword: EditText
+    private lateinit var editName: EditText
+    private lateinit var signupBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,19 +26,30 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.frament_log_in, container, false)
+        val view = inflater.inflate(R.layout.frament_sign_up, container, false)
 
         editEmail = view.findViewById(R.id.editTextEmail)
+        editName = view.findViewById(R.id.editTextName)
         editPassword = view.findViewById(R.id.editTextPassword)
-        loginBtn = view.findViewById(R.id.loginBtn)
-        forgotPasswordBtn = view.findViewById(R.id.forgotPasswordBtn)
+        editRePassword = view.findViewById(R.id.editTextRePassword)
         signupBtn = view.findViewById(R.id.signupBtn)
 
-        loginBtn.setOnClickListener {
+        signupBtn.setOnClickListener {
             val email = editEmail.text.toString().trim()
             val password = editPassword.text.toString().trim()
+            val rePassword = editRePassword.text.toString().trim()
             if (email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(requireActivity(), "Enter email and password", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+
+            if (password != rePassword) {
+                Toast.makeText(
+                    requireActivity(),
+                    "Passwords should be the same",
+                    Toast.LENGTH_SHORT
+                )
                     .show()
                 return@setOnClickListener
             }
@@ -56,7 +66,7 @@ class LoginFragment : Fragment() {
 
         signupBtn.setOnClickListener {
             parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, SignupFragment())
+                .replace(R.id.fragment_container, HomeFragment())
                 .commit()
         }
 
